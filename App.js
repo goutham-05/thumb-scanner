7d2ac3dc7352f91615d17a33bef47053b1e45b48
goutham-05 <goutham1494@gmail.com>
2023-01-04 00:01:35 +0000

    working setup

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback, Image } from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
-export default function App() {
+
+
+var yourPicture = require('./assets/splash.png');
+
+
+
+const App = () => {
+  const [progressValue, setProgressValue] = useState(0);
+
+  const _onLongPress = () => {
+    if (progressValue !== 100) {
+      setProgressValue(progressValue + 10);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <TouchableWithoutFeedback
+      style={styles.container}
+      onPressIn={_onLongPress}
+    >
+      <View style={styles.container}>
+
+        <CircularProgress
+          value={progressValue}
+          duration={1000}
+          title={<>
+            <Text
+
+            >sdsd
+              <Image
+              source={yourPicture} />
+
+              
+            </Text>
+          </>}
+          strokeColorConfig={[
+            { color: 'red', value: 0 },
+            { color: 'skyblue', value: 50 },
+            { color: 'yellowgreen', value: 100 },
+          ]}
+
+        />
+      </View>
+    </TouchableWithoutFeedback>
+
     </View>
+
+
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 4,
+    justifyContent: "center",
   },
+  text: {
+    fontSize: 16
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6
+  },
+  logBox: {
+    padding: 20,
+    margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#f0f0f0',
+    backgroundColor: '#f9f9f9'
+  }
 });
+
+export default App;
